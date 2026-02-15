6b284c5f0a4bb2e3b418d057d5a64753a173def0
dot <rawatkushagra33@gmail.com>
2026-02-15 11:50:52 +0530

    re: Theme Change

--- init.el
+++ init.el
@@ -152,7 +152,44 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-vivendi-tritanopia))
  '(custom-safe-themes
-   '("e9835d67c171416877752f0aa0293b1f2070e5e011fb76419bae0adead7cbd45"
+   '("677e3d3d9ca354bfa2422287bc8ef048a759cd5009493e75197d3a3ab3590c58"
+     "1c725e4aee133f350459e0692f2b885c3444982bf6e8647b05d1cfc519760d9f"
+     "4515e88dea04cac581b05b1893f461169fad58a1bd1f255605e0e586a354f7bc"
+     "5db7ff8db7fe64558355dba6314cb73008eb7cb1a4ca4552d69fbe588ef4c224"
+     "2b9e0d7bceebee7473c100ecbf2c76a65b6e2129c73775f5949e39a677fa621f"
+     "be2cf7aada403c8ce552e29fce64b1cda28db4cdc72c1152a6e8989c8ac69847"
+     "76829ecd2a3810c7a20b4163c8b3dbc8c4faf1674bb568e5150dd8d24fef25bf"
+     "f0ddff48ec78dec9ac9ea3236e7208ef58a86d445207a674523c0eba8e9743b0"
+     "749a7bb14efeb8b6c9b251c7a771ab7de500b247eb35f69bfccbdfca27e0602c"
+     "e684e71b7276a85c0ec8ee8f3ff4bc6b61dbc1694288ad049e062029fc072e54"
+     "0da9f48243b77c83719c569efd88a7e47c1d8a3f2887838408745d2d52fad8a8"
+     "01bb579ed8a5b4a77490798626a96ae153288b8c2709e291162cfcc2e0aad647"
+     "6a95b0faf6cee6adfda34cdfadb2fed6f4157a1d49aabef8cc9b94c187d69a1d"
+     "5acb2e842cccd8e19e3f263a7e27345b7c12024b80cfad00094c9da54cc0a0ed"
+     "4e6e3a04936e001ca758dce1bfa5b54fb601683a7361c30d754347a75f321d28"
+     "1b2727001d06f63916118b811da49a9344582c16f9e2d7e26657634712886775"
+     "dc8d63cd0514d058bdf34774163709834519350c1a80dc04deaa58085a8810a5"
+     "6aff83667a3bad8db2f55d69cf595f58b56af233f116ec24d93fe2aa51033eef"
+     "03ffccc093c553a238a54fea13f2056749d83c24e65940f8d4bdb7135f1199a5"
+     "f5ab1ad901eb430cdcd9b2a6824e94ff384172a9492ff7a88fe989ee2d583f09"
+     "6965a903ced31bd58caddb7e7035aadc47f8b0a5c57f246b698be2dfdfed2c4e"
+     "6d58e2b15bdf919fb3f975572af5de59556507b4addf71d8a0d4da3b5af1e830"
+     "cf0e1f8a027d3a38602ce6628399db5550ad7f0598c01979e01ab45b8a631639"
+     "51caf9bf88aba940d98c96add138d83317d50eae4b8526612184e93473252d54"
+     "0206a1960f04896d7f5144e2e8ac0119bb63df78deec362337e7cc3003b946e9"
+     "98f2052cba13ccbf7dc91e6ce4dbc02fc9f6421782c11f8403504222f4d423f7"
+     "ed2efc874021b54144b9ed9874e4d9c036090488aeabe19c9ac28576a162ec12"
+     "1b74dd5576787105a0efcd6e8fed74755bb3fd234aaa5d500e21ddbd56c2661a"
+     "0f738dce3f831b6d64ee3e98052bdea663b74d5149dcbbf555327dcb4517fc08"
+     "2c7dc80264de0ba9409d4ebb3c7b31cf8e4982015066174c786f16a672db71b2"
+     "516ec39655c85f346393f5d93e0f03602b6bfc33335bf2fd673016c9c4cdc69e"
+     "22faff66975354a3dd145ae13934758d45930ad09cbbe8bd1ed74224e4aa5684"
+     "0a8cf72fd94bfb67dd72dc085538b39ea47aeae8bfc2b8545c0d3c99c339c204"
+     "546f3e8c4cb46043df1f646322c4b57049fc4c31fdf96e41db077c3408660057"
+     "578002ec7093cedeb7d279794492b19ad7413cca3e16286e776168eb544798c4"
+     "97e5f82e4e93c3a03fc823673d55cbb7e2c9cc27a6ac419b11c23ef4a2d0de26"
+     "9819b94dc38ee935362d4bf1c1198ebb8ebeabb5e8c85a78436565720d91c0d7"
+     "e9835d67c171416877752f0aa0293b1f2070e5e011fb76419bae0adead7cbd45"
      "f205f6f7fc55a6b1e206e88445931ac46ec9a70f124291667809707ddbe63c04"
      "8fbf2d585f1138caaafa9e523fa3a20614c1d1dcc6002c9808c3e40028e21df4"
      "a75fc55d480accd6f651d1bae492b7ab4c5b28894350b63ef39bf947bb8fd453"
@@ -186,9 +223,10 @@
      "c9e63647d90e7dc59d52c176fbfd46fd2cfed275cd2bad9b29b6cf620d3389b6"))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(calibredb company drag-stuff flycheck go-mode helm-lsp ligature
-	       lsp-treemacs lsp-ui magit modus-themes multiple-cursors
-	       rust-mode smartparens web-mode yasnippet)))
+   '(calibredb company drag-stuff ef-themes flycheck go-mode helm-lsp
+	       ligature lsp-treemacs lsp-ui magit modus-themes
+	       multiple-cursors pdf-tools rust-mode smartparens
+	       web-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
