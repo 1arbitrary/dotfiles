3873a5c551181caeb35ab1d4f5f51427b0db5e5f
dot <rawatkushagra33@gmail.com>
2026-05-31 11:27:23 +0530

    added niri,foot,waybar, fuzzel and update init.el

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
 (which-key-mode t)
-(set-frame-font "IosevkaTerm Nerd Font 16" nil t)
+(set-frame-font "IosevkaNerdFont 16" nil t)
 
 					; Turns on displaying numbers 
 (global-display-line-numbers-mode 1)
@@ -85,41 +85,23 @@
 
 (setq dictionary-server "dict.org")
 
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(github-dark-vscode))
+ '(custom-enabled-themes '(gruvbox-light-medium))
  '(custom-safe-themes
-   '("b9f44212b4be6f0466811c5d8a297dda3c40dbf4c4cfd97c1686fceb2043b617"
-     "59cb0b29e1b279f887d13214bb91c20cce880c3b9647d3e74f66d1b18756c682"
-     "a94e15f0a3fd9e86430b4b8dadffc58faa0310168bcbc4fd3c1b687cf25e1f5d"
-     "dfa1e5afbab42ab2924102e11672a2731c53347ce078b734d9816f1d4429f68a"
-     "1b0db76a9b417336906972209ca4b2bf1f0755f6c2013b772cb26117cc326eda"
-     "b3c54ff374d12c2e45c621d723a0f6dfb6aab2bb5478a0c4f8b7590adaa23892"
-     "13794b95ba118a1e0f60fe880be9c8a79d881aeb2203656a092f462b2b601629"
-     "d10c58f4a53b7d4760022fdf67734865c290e40ef5dfa597a653e14b810a7d07"
-     "70901ffb0f502ec640c30f96a626e66a024af80ebb836eb0cdbe42c06446951b"
-     "b97004bc28b4d931b589c3453ff83852c28bb009df3421ef0b8b2752e43c67fa"
-     "5d09ffe65f025fd9f51304aad148140d18ce14411d20b9e563f27581b0305af9"
-     "d81457b350b6b3ba9cf98b589b76b16faef3918e5857d302839484bdef9f58ac"
-     "62e5ee6ff1f9437e7b9010d4575d0415237dfbee5fd64b2f2b5597969499fec2"
-     "d20b3f5c1adf9cf2e2ad40a0f6172b32d53faa0489478cf359d2bdf432b9d8a4"
-     "ac7b410453c725606757b5aa46df55be487d53b9fa50b13ea548e9642c32377b"
-     "cbd85ab34afb47003fa7f814a462c24affb1de81ebf172b78cb4e65186ba59d2"
-     "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7"
-     "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c"
-     "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae"))
- '(package-selected-packages
-   '(company github-dark-vscode-theme go-mode ligature multiple-cursors
-	     rust-mode yasnippet)))
+   '("18a1d83b4e16993189749494d75e6adb0e15452c80c431aca4a867bcc8890ca9"
+     "51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3"
+     "75b371fce3c9e6b1482ba10c883e2fb813f2cc1c88be0b8a1099773eb78a7176"
+     "5aedf993c7220cbbe66a410334239521d8ba91e1815f6ebde59cecc2355d7757"
+     default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
